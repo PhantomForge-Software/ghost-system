adc7708833ccb51e7862bf5861282372ff0fe549
Orim12 <miro.vaassen@gmail.com>
2025-03-20 12:02:02 +0100

    Add hitbox indicators and improve drag-and-drop behavior in MainScene

--- js/MainScene.js
+++ js/MainScene.js
@@ -12,21 +12,39 @@ class MainScene extends Phaser.Scene {
             this.cards.add(card);
         }
 
+        // Create hitbox indicators closer to the cards
+        this.leftHitbox = this.add.rectangle(this.scale.width * 0.1, this.scale.height / 2, this.scale.width * 0.5, this.scale.height, 0xff0000, 0.5).setOrigin(0.5, 0.5);
+        this.rightHitbox = this.add.rectangle(this.scale.width * 0.9, this.scale.height / 2, this.scale.width * 0.5, this.scale.height, 0x00ff00, 0.5).setOrigin(0.5, 0.5);
+
         this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
             gameObject.x = dragX;
             gameObject.y = dragY;
         });
 
         this.input.on('dragend', function (pointer, gameObject) {
-            if (gameObject.x > window.innerWidth * 0.75) {
-                gameObject.destroy();
-            } else if (gameObject.x < window.innerWidth * 0.25) {
-                gameObject.destroy();
+            if (gameObject.x > this.scale.width * 0.8) {
+                this.tweens.add({
+                    targets: gameObject,
+                    alpha: 0,
+                    duration: 500,
+                    onComplete: function () {
+                        gameObject.destroy();
+                    }
+                });
+            } else if (gameObject.x < this.scale.width * 0.5) {
+                this.tweens.add({
+                    targets: gameObject,
+                    alpha: 0,
+                    duration: 500,
+                    onComplete: function () {
+                        gameObject.destroy();
+                    }
+                });
             } else {
-                gameObject.x = window.innerWidth / 2;
-                gameObject.y = window.innerHeight / 2;
+                gameObject.x = this.scale.width / 2;
+                gameObject.y = this.scale.height / 2;
             }
-        });
+        }, this);
     }
 
     update() {
