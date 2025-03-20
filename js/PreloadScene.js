8b5178736c363ce4fc27c8285aceb44e4550869e
Orim12 <miro.vaassen@gmail.com>
2025-03-20 13:24:58 +0100

    Fix image paths in PreloadScene for consistency

--- js/PreloadScene.js
+++ js/PreloadScene.js
@@ -4,14 +4,14 @@ class PreloadScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('deepfake_1', '..\\assets\\img\\DF_1.png');
-        this.load.image('deepfake_2', '..\\assets\\img\\DF_2.png');
-        this.load.image('deepfake_3', '..\\assets\\img\\DF_3.png');
-        this.load.image('deepfake_4', '..\\assets\\img\\DF_4.png');
-        this.load.image('deepfake_5', '..\\assets\\img\\DF_5.png');
-        this.load.image('deepfake_6', '..\\assets\\img\\DF_6.png');
-        this.load.image('normal_1', '..\\assets\\img\\R_1.png');
-        this.load.image('normal_2', '..\\assets\\img\\R_2.png');
+        this.load.image('deepfake_1', '../assets/img/DF_1.png');
+        this.load.image('deepfake_2', '../assets/img/DF_2.png');
+        this.load.image('deepfake_3', '../assets/img/DF_3.png');
+        this.load.image('deepfake_4', '../assets/img/DF_4.png');
+        this.load.image('deepfake_5', '../assets/img/DF_5.png');
+        this.load.image('deepfake_6', '../assets/img/DF_6.png');
+        this.load.image('normal_1', '../assets/img/R_1.png');
+        this.load.image('normal_2', '../assets/img/R_2.png');
     }
 
     create() {
