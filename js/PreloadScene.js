81d93bc6ec46e52e7e5e07318d44a501d10420c7
Orim12 <miro.vaassen@gmail.com>
2025-03-21 09:59:19 +0100

    Update image paths in PreloadScene to use direct URLs

--- js/PreloadScene.js
+++ js/PreloadScene.js
@@ -4,22 +4,22 @@ class PreloadScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('deepfake_1', '../assets/img/DF_1.png');
-        this.load.image('deepfake_2', '../assets/img/DF_2.png');
-        this.load.image('deepfake_3', '../assets/img/DF_3.png');
-        this.load.image('deepfake_4', '../assets/img/DF_4.png');
-        this.load.image('deepfake_5', '../assets/img/DF_5.png');
-        this.load.image('deepfake_6', '../assets/img/DF_6.png');
-        this.load.image('deepfake_7', '../assets/img/DF_7.png');
-        this.load.image('deepfake_8', '../assets/img/DF_8.png');
-        this.load.image('normal_1', '../assets/img/R_1.png');
-        this.load.image('normal_2', '../assets/img/R_2.jpeg');
-        this.load.image('normal_3', '../assets/img/R_3.png');
-        this.load.image('normal_4', '../assets/img/R_4.jpeg');
-        this.load.image('normal_5', '../assets/img/R_5.jpeg');
-        this.load.image('normal_6', '../assets/img/R_6.png');
-        this.load.image('normal_7', '../assets/img/R_7.jpeg');
-        this.load.image('normal_8', '../assets/img/R_8.jpeg');
+        this.load.image('deepfake_1', 'https://raw.githubusercontent.com/PhantomForge-Software/liefde-liegt/main/assets/img/DF_1.png');
+        this.load.image('deepfake_2', 'https://raw.githubusercontent.com/PhantomForge-Software/liefde-liegt/main/assets/img/DF_2.png');
+        this.load.image('deepfake_3', 'https://raw.githubusercontent.com/PhantomForge-Software/liefde-liegt/main/assets/img/DF_3.png');
+        this.load.image('deepfake_4', 'https://raw.githubusercontent.com/PhantomForge-Software/liefde-liegt/main/assets/img/DF_4.png');
+        this.load.image('deepfake_5', 'https://raw.githubusercontent.com/PhantomForge-Software/liefde-liegt/main/assets/img/DF_5.png');
+        this.load.image('deepfake_6', 'https://raw.githubusercontent.com/PhantomForge-Software/liefde-liegt/main/assets/img/DF_6.png');
+        this.load.image('deepfake_7', 'https://raw.githubusercontent.com/PhantomForge-Software/liefde-liegt/main/assets/img/DF_7.png');
+        this.load.image('deepfake_8', 'https://raw.githubusercontent.com/PhantomForge-Software/liefde-liegt/main/assets/img/DF_8.png');
+        this.load.image('normal_1', 'https://raw.githubusercontent.com/PhantomForge-Software/liefde-liegt/main/assets/img/R_1.png');
+        this.load.image('normal_2', 'https://raw.githubusercontent.com/PhantomForge-Software/liefde-liegt/main/assets/img/R_2.jpeg');
+        this.load.image('normal_3', 'https://raw.githubusercontent.com/PhantomForge-Software/liefde-liegt/main/assets/img/R_3.png');
+        this.load.image('normal_4', 'https://raw.githubusercontent.com/PhantomForge-Software/liefde-liegt/main/assets/img/R_4.jpeg');
+        this.load.image('normal_5', 'https://raw.githubusercontent.com/PhantomForge-Software/liefde-liegt/main/assets/img/R_5.jpeg');
+        this.load.image('normal_6', 'https://raw.githubusercontent.com/PhantomForge-Software/liefde-liegt/main/assets/img/R_6.png');
+        this.load.image('normal_7', 'https://raw.githubusercontent.com/PhantomForge-Software/liefde-liegt/main/assets/img/R_7.jpeg');
+        this.load.image('normal_8', 'https://raw.githubusercontent.com/PhantomForge-Software/liefde-liegt/main/assets/img/R_8.jpeg');
     }
 
     create() {
